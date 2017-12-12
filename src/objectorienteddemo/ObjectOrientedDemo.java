dac3af50fe30c6cf1251dc197453f339663c84e1
Nicol <Nicol@DESKTOP-UBBP8C8.attlocal.net>
2017-12-12 01:34:07 -0500

    commit

---
+++ src/objectorienteddemo/ObjectOrientedDemo.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package objectorienteddemo;
+
+/**
+ *
+ * @author Nicol
+ */
+public class ObjectOrientedDemo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
