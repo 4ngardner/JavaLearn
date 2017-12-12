e5ded853b999ea918a2f4c47ba1cf30230dbe342
ngardner3 <ngardner3@CSCUSAAH763070.amer.globalcsc.net>
2017-12-12 08:46:23 -0500

    commit work desktop

---
+++ src/objectorienteddemo/ObjectOrientedDemo.java
@@ -0,0 +1,17 @@
+
+package objectorienteddemo;
+
+/**
+ *
+ * @author ngardner3
+ */
+public class ObjectOrientedDemo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
