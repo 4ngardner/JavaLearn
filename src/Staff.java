81d733e6bd5f081261789cf49e2aa5e8bfe18da8
ngardner3 <ngardner3@CSCUSAAH763070.amer.globalcsc.net>
2017-12-13 19:44:31 -0500

    commit work desktop

--- src/Staff.java
+++ src/Staff.java
@@ -27,7 +27,25 @@ public class Staff {
          
     }
         
-    
+     public int calculatePay(int bonus, int allowance)
+{
+    printMessage();
+    if (hoursWorked>0)
+         return hoursWorked * hourlyRate + bonus + allowance;
+    else
+         return 0;
+}
+ public void setHoursWorked(int hours)
+ {
+     if (hours>0)
+         hoursWorked = hours;
+     else
+     {
+         System.out.println("Error: hoursWorked Cannot be Smaller than Zero");
+         System.out.println("Error: HoursWorked is not updated");
+     }
+ }
+}
     
     
     
