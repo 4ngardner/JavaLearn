fdf77ad6f943639dd58cc6624bd1444f9d622fbb
ngardner3 <ngardner3@CSCUSAAH763070.amer.globalcsc.net>
2017-12-13 19:34:08 -0500

    commit work desktop

---
+++ src/Staff.java
@@ -0,0 +1,38 @@
+
+/**
+ *
+ * @author ngardner3
+ */
+public class Staff {
+    private String nameOfStaff;
+    private final int hourlyRate = 30;
+    private int hoursWorked;
+    
+    public void printMessage()
+    {
+      System.out.println("Calculating Pay...");
+
+    }
+    
+    public int calculatePay()
+    {
+      printMessage();
+              
+      int staffPay;
+      staffPay = hoursWorked * hourlyRate;
+          if (hoursWorked > 0)
+              return staffPay;
+          else 
+              return -1;
+         
+    }
+        
+    
+    
+    
+    
+    
+    }
+    
+
+
