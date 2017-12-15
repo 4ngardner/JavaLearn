b03ab6284b75430187d48d73d791f4204f1d3406
Nicol <Nicol@DESKTOP-UBBP8C8.attlocal.net>
2017-12-15 16:29:17 -0500

    commit

--- src/objectorienteddemo/ObjectOrientedDemo.java
+++ src/objectorienteddemo/ObjectOrientedDemo.java
@@ -11,7 +11,10 @@ public class ObjectOrientedDemo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Staff staff1 = new Staff ("Nicole");
+        staff1.setHourWorked(160);
+        int pay = staff1.calculatePay(1000, 400);
+        System.out.println("Pay = " + pay);
     }
     
 }
